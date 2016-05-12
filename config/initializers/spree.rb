23fc6619112714566bb7fa0bfab77566136df9b8
Christopher Rijos <rijos.christopher@gmail.com>
2016-05-12 17:44:31 +0000

    Added new file path for spree s3 config

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,34 +10,35 @@
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
 Spree.config do |config|
-  # Example:
-  # Uncomment to stop tracking inventory levels in the application
-  # config.track_inventory_levels = false
+   #S3 configuration
+      if Rails.env.production? then
+           #production. Store images on S3.
+           # development will default to local storage
+          attachment_config = {
+          s3_credentials: {
+            access_key_id: ENV["S3_KEY"],
+            secret_access_key: ENV["S3_SECRET"],
+            bucket: ENV["S3_BUCKET"],
+          },
+
+
+          storage:        :s3,
+          s3_headers:     { "Cache-Control" => "max-age=31557600" },
+          s3_protocol:    "https",
+          bucket:         ENV["S3_BUCKET"],
+
+          path:          ":rails_root/public/:class/:attachment/:id/:style/:basename.:extension",
+          default_url:   "/:class/:attachment/:id/:style/:basename.:extension",
+          default_style: "product",
+          }
+
+          attachment_config.each do |key, value|
+               Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
+          end
+     end
 end
 
 
-Rails.env.production?
-  attachment_config = {
-    s3_credentials: {
-      access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      bucket:            ENV['S3_BUCKET_NAME']
-    },
-
-    storage:        :s3,
-    s3_headers:     { "Cache-Control" => "max-age=31557600" },
-    s3_protocol:    "https",
-    bucket:         ENV['S3_BUCKET_NAME'],
-    url:            ":s3_domain_url",
-
-    path:           "/:class/:id/:style/:basename.:extension",
-    default_url:    "/:class/:id/:style/:basename.:extension",
-    default_style:  "product"
-  }
-
-  attachment_config.each do |key, value|
-    Spree::Image.attachment_definitions[:attachment][key.to_sym] = value
-  end
-end
+  
 
 Spree.user_class = "Spree::User"
