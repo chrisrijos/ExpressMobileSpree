ffeb8cd2b3d500d3ee6f797ebca8cd232fd03c10
Christopher Rijos <rijos.christopher@gmail.com>
2016-05-27 21:37:29 +0000

    Removed header from a few of the pages

--- app/controllers/spree/home_controller.rb
+++ app/controllers/spree/home_controller.rb
@@ -4,6 +4,7 @@ module Spree
     respond_to :html
 
     def index
+      @skip_header = true
       @searcher = build_searcher(params.merge(include_images: true))
       @products = @searcher.retrieve_products.includes(:possible_promotions)
       @taxonomies = Spree::Taxonomy.includes(root: :children)
